1f87b2ac609a3ad268ffcc0d50b3cef692684de2
KEVPC\keven <kevenlessard23@gmail.com>
2022-11-15 01:40:45 -0500

    [Summary] -Added player_two -Added InputManager

--- TopDownCarRacing/Assets/Scripts/TopDownCarController.cs
+++ TopDownCarRacing/Assets/Scripts/TopDownCarController.cs
@@ -1,58 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class TopDownCarController : MonoBehaviour
 {
-    
+    public InputManager inputManager;
     public float maxSpeed;
     public float acceleration;
     public float steering;
  
-    private Rigidbody2D rb;
-    private float currentSpeed;
-
-    private float accelerationInput;
-    private float steeringInput;
+    private Rigidbody2D _rb;
     private void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
     }
  
     private void FixedUpdate()
     {
-        GetInput();
+        Vector2 inputs = inputManager.GetInput(this.name);
 
         // Calculate speed from input and acceleration (transform.up is forward)
-        Vector2 speed = transform.up * (accelerationInput * acceleration);
-        rb.AddForce(speed);
+        Vector2 speed = transform.up * (inputs.x * acceleration);
+        _rb.AddForce(speed);
  
         // Create car rotation
-        float direction = Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.up));
+        float direction = Vector2.Dot(_rb.velocity, _rb.GetRelativeVector(Vector2.up));
         if (direction >= 0.0f)
         {
-            rb.rotation += steeringInput * steering * (rb.velocity.magnitude / maxSpeed);
+            _rb.rotation += inputs.y * steering * (_rb.velocity.magnitude / maxSpeed);
         }
         else
         {
-            rb.rotation -= steeringInput * steering * (rb.velocity.magnitude / maxSpeed);
+            _rb.rotation -= inputs.y * steering * (_rb.velocity.magnitude / maxSpeed);
         }
  
         // Change velocity based on rotation
-        float driftForce = Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.left)) * 2.0f;
+        float driftForce = Vector2.Dot(_rb.velocity, _rb.GetRelativeVector(Vector2.left)) * 2.0f;
         Vector2 relativeForce = Vector2.right * driftForce;
-        rb.AddForce(rb.GetRelativeVector(relativeForce));
+        _rb.AddForce(_rb.GetRelativeVector(relativeForce));
  
         // Force max speed limit
-        if (rb.velocity.magnitude > maxSpeed)
+        if (_rb.velocity.magnitude > maxSpeed)
         {
-            rb.velocity = rb.velocity.normalized * maxSpeed;
+            _rb.velocity = _rb.velocity.normalized * maxSpeed;
         }
-        currentSpeed = rb.velocity.magnitude;
-    }
-
-    private void GetInput()
-    {
-        
     }
 }
